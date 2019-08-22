610439386ae3d31c28202b0dde65517a7c10df53
Josh Bullers <joshbullers@Joshs-MacBook-Pro.local>
2019-08-21 20:47:00 -0700

    updated grammer for parser

--- lisp-in-c/polish/parsing.c
+++ lisp-in-c/polish/parsing.c
@@ -31,9 +31,19 @@ int main(int argc, char** argv)
     mpc_parser_t* Operater  = mpc_new("operator");
     mpc_parser_t* Expr      = mpc_new("expr");
     mpc_parser_t* Lispy     = mpc_new("lispy");
+
+    /* Define them with the following language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                                                   \
+            number   : /[+-]?([0-9]*[.])?[0-9]+/ ;                                          \
+            operator : '+'| '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\" ; \
+            expr     : <number> | '(' <operator> <expr>+ ')' ;                              \
+            lispy    : /^/ <operator> <expr>+ /$/ ;                                         \
+        ",
+        Number, Operater, Expr, Lispy);
     
     /* Print Version and Exit Information */
-    puts("Lispy Version 0.0.0.0.1");
+    puts("Lispy Version 0.0.0.0.2");
     puts("Press Ctrl+c to Exit\n");
 
     /* In a never ending loop */
@@ -45,12 +55,26 @@ int main(int argc, char** argv)
         /* Add input to history */
         add_history(input);
 
-        /* Echo input back to user */
-        printf("No you're a %s\n", input);
+        /* Attempt to parse the user input */
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r))
+        {
+            /* On the success print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        }
+        else
+        {
+            /* Otherwise print the error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         /* Free retrieed input */
         free(input);
     }
     
+    mpc_cleanup(4, Number, Operater, Expr, Lispy);
+
     return 0;
 }
